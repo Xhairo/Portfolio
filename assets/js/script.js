34ce1ace296dbab4c05288aa5e93be7a743d56f3
Xhairo <xhairo94@gmail.com>
2024-07-15 16:14:52 -0400

    new project added, some links added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,33 @@
 const projects = {
-  project1: {
+  project3: {
     title: "Shoot Fast Cowboy",
     description:
       "Este juego fue un proyecto personal en el año 2023 donde decidi realizarlo para un evento llamado Demoday.",
     tools: ["Unity", "Blender", "Csharp", "Firebase"],
     image: "assets/imagenes/Proyectos/proyect1.png",
     video: "https://www.youtube.com/embed/rck4Krg_cII",
-    repo: "",
-    ver: "",
+    repo: "https://github.com/Xhairo/ShootFastCowboy",
+    ver: "https://xhairo.itch.io/shootfastcowboy",
   },
-  project2: {
+  project4: {
     title: "reWorld",
-    description: "GameJam GamerCity con el tema de Ciudad Sustentable",
+    description: "GameJam GamerCity 2023 con el tema de Ciudad Sustentable",
     tools: ["Unity", "Blender", "Csharp"],
     image: "assets/imagenes/Proyectos/proyect2.jpg",
     video: "https://www.youtube.com/embed/BgXaboXJYD8",
     repo: "",
-    ver: "",
+    ver: "https://xhairo.itch.io/reworld",
   },
-  project3: {
+  project5: {
     title: "Master of Magic",
-    description:
-      "Mi Primer Videojuego donde fue mi inicio en el mundo de la programacion con bloques de logica",
-    tools: ["Unity", "Blender", "Csharp"],
+    description: `Mi Primer Videojuego realizado en 2014, fue mi inicio en el mundo de la programacion con bloques de logica <a href="assets/imagenes/gdd.pdf">GDD</a>`,
+    tools: ["Construct"],
     image: "assets/imagenes/Proyectos/masterofmagic.png",
     video: "",
     repo: "",
-    ver: "",
+    ver: "https://xhairo.itch.io/master-of-magic",
   },
-  project4: {
+  project2: {
     title: "E-Commerce",
     description:
       "e-commerce, de videojuegos con un estilo visual de Cyberpunk 2077",
@@ -38,15 +37,25 @@ const projects = {
     repo: "",
     ver: "",
   },
-  project5: {
+  project1: {
     title: "BiciConectados",
     description:
       "Red Social que contiene un mapa Interactivo para realizar rutas que incentivar el el uso de la bicicleta como medio de transporte.",
     tools: ["React", "Node", "Spring", "Git", "GitHub", "java", "mysql"],
     image: "assets/imagenes/Proyectos/BiciConectados.png",
     video: "https://www.youtube.com/embed/zQ3ThVIiI3Q",
-    repo: "",
+    repo: "https://github.com/jupitaa/FrontBiciconectados",
     ver: "",
+  },
+  project6: {
+    title: "Space Tournament",
+    description:
+      "Juego Gatcha de futbol americano estilo anime espacial, realizado el 2017 para mi titulacion",
+    tools: ["Unity", "Csharp"],
+    image: "assets/imagenes/Proyectos/spaceTournament.png",
+    video: "",
+    repo: "",
+    ver: "https://xhairo.itch.io/space-tournament",
   },
   // Add other projects similarly
 };
@@ -82,7 +91,7 @@ function showMainCard(projectId) {
         ${
           project.video
             ? `<iframe width="100%" height="150" src="${project.video}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
-            : `<img src="${project.image}" alt="${project.title} height="100%" width="100%">`
+            : `<img src="${project.image}" alt="${project.title}" height="100%" width="100%">`
         }
         <h2>${project.title}</h2>
         <p>${project.description}</p>
@@ -91,8 +100,20 @@ function showMainCard(projectId) {
             ${toolsHtml}
         </div>
         <div class="links">
-          <a href="${project.repo}">GitHub</a>
-          <a href="${project.ver}">Ver</a>
+          ${
+            project.repo
+              ? ` <a href="${project.repo}" target="_blank">
+                GitHub
+              </a>`
+              : ""
+          }
+          ${
+            project.ver
+              ? ` <a href="${project.ver} " target="_blank">
+                Ver
+              </a>`
+              : ""
+          }
         </div>
     `;
 
