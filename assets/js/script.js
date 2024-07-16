865afd4f1ab6a410ac37e2dea5d242e324516255
Xhairo <xhairo94@gmail.com>
2024-07-16 15:32:23 -0400

    e-commerce added, links style modify, gramatic issues fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,60 +2,66 @@ const projects = {
   project3: {
     title: "Shoot Fast Cowboy",
     description:
-      "Este juego fue un proyecto personal en el año 2023 donde decidi realizarlo para un evento llamado Demoday.",
+      "Este juego fue un proyecto personal en el año 2023 donde decidí realizarlo para un evento llamado Demo day.",
     tools: ["Unity", "Blender", "Csharp", "Firebase"],
     image: "assets/imagenes/Proyectos/proyect1.png",
     video: "https://www.youtube.com/embed/rck4Krg_cII",
     repo: "https://github.com/Xhairo/ShootFastCowboy",
-    ver: "https://xhairo.itch.io/shootfastcowboy",
+    ver: "",
+    jugar: "https://xhairo.itch.io/shootfastcowboy",
   },
   project4: {
     title: "reWorld",
-    description: "GameJam GamerCity 2023 con el tema de Ciudad Sustentable",
+    description: "GameJam Gamer City 2023 con el tema de Ciudad Sustentable.",
     tools: ["Unity", "Blender", "Csharp"],
     image: "assets/imagenes/Proyectos/proyect2.jpg",
     video: "https://www.youtube.com/embed/BgXaboXJYD8",
     repo: "",
-    ver: "https://xhairo.itch.io/reworld",
+    ver: "",
+    jugar: "https://xhairo.itch.io/reworld",
   },
   project5: {
     title: "Master of Magic",
-    description: `Mi Primer Videojuego realizado en 2014, fue mi inicio en el mundo de la programacion con bloques de logica. <br><br> <a class="clikeable" href="assets/imagenes/gdd.pdf">GDD</a>`,
+    description: `Mi Primer Videojuego realizado en 2014, fue mi inicio en el mundo de la programación con bloques de lógica. <br><br> <a class="clikeable" href="assets/imagenes/gdd.pdf">GDD</a>`,
     tools: ["Construct"],
     image: "assets/imagenes/Proyectos/masterofmagic.png",
     video: "",
     repo: "",
-    ver: "https://xhairo.itch.io/master-of-magic",
+    ver: "",
+    jugar: "https://xhairo.itch.io/master-of-magic",
   },
   project2: {
     title: "E-Commerce",
     description:
-      "e-commerce, de videojuegos con un estilo visual de Cyberpunk 2077",
+      "E-commerce, de videojuegos con un estilo visual de Cyberpunk 2077, aun en desarrollo.",
     tools: ["React", "Node", "Git", "JS"],
     image: "assets/imagenes/Proyectos/e-commerce.png",
     video: "",
     repo: "https://github.com/Xhairo/e-commerce",
-    ver: "",
+    ver: "https://xhairo.github.io/e-commerce/",
+    jugar: "",
   },
   project1: {
     title: "BiciConectados",
     description:
-      "Red Social que contiene un mapa Interactivo para realizar rutas que incentivar el el uso de la bicicleta como medio de transporte.",
+      "Red Social que contiene un mapa Interactivo para realizar rutas que incentivar el uso de la bicicleta como medio de transporte.",
     tools: ["React", "Node", "Spring", "Git", "GitHub", "java", "mysql"],
     image: "assets/imagenes/Proyectos/biciconectados.png",
     video: "https://www.youtube.com/embed/zQ3ThVIiI3Q",
     repo: "https://github.com/jupitaa/FrontBiciconectados",
     ver: "",
+    jugar: "",
   },
   project6: {
     title: "Space Tournament",
     description:
-      "Juego Gatcha de futbol americano estilo anime espacial, realizado el 2017 para mi titulacion",
+      "Juego Gacha de futbol americano estilo anime espacial, realizado el 2017 para mi titulación.",
     tools: ["Unity", "Csharp"],
     image: "assets/imagenes/Proyectos/spaceTournament.png",
     video: "",
     repo: "",
-    ver: "https://xhairo.itch.io/space-tournament",
+    ver: "",
+    jugar: "https://xhairo.itch.io/space-tournament",
   },
   // Add other projects similarly
 };
@@ -110,6 +116,13 @@ function showMainCard(projectId) {
           ${
             project.ver
               ? ` <a href="${project.ver} " target="_blank">
+                Ver
+              </a>`
+              : ""
+          }
+          ${
+            project.jugar
+              ? ` <a href="${project.jugar} " target="_blank">
                 Jugar
               </a>`
               : ""
